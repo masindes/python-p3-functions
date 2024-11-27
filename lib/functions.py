b7a0fc49f90519a4c88acb9fe6257649bf1e150b
masinde sylvester <masinde.sylvester@yahoo.com>
2024-11-27 23:29:03 +0300

    initial commit

--- lib/functions.py
+++ lib/functions.py
@@ -5,10 +5,10 @@ def greet_programmer():
     
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
     pass
